e160464c70c01a36e82d5027d1f6d540f3a59ea3
Natalia Trevisan da Silva <natalias@lnxcit018660.cit>
2018-04-26 00:20:13 -0300

    Creation of new report version

--- src/main/java/stepDefinition/TestRunner.java
+++ src/main/java/stepDefinition/TestRunner.java
@@ -1,16 +1,25 @@
 package stepDefinition;
 
+import java.io.File;
+
+import org.junit.AfterClass;
 import org.junit.runner.RunWith;
+
+import com.cucumber.listener.Reporter;
+
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
 		features = "src/test/java/features",
-		glue= {"stepDefinition"},
-		plugin = { "pretty", "html:target/cucumber-reports" },
+		glue = {"stepDefinition"},
+		plugin = { "com.cucumber.listener.ExtentCucumberFormatter:target/report/index.html" },
 		monochrome = true)
  
 public class TestRunner {
-
+	@AfterClass
+	public static void writeExtentReport() {
+		Reporter.loadXMLConfig(new File("reportConfig.xml"));
+	}
 }
